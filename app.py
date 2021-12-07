e7cb4927a25f41505db213093fdda71884ba61e3
AndrewH1188 <andrewh1188@gmail.com>
2021-12-07 12:35:18 +0000

    Delete task from the database

--- app.py
+++ app.py
@@ -130,6 +130,13 @@ def edit_task(task_id):
     return render_template("edit_task.html", task=task, categories=categories)
 
 
+@app.route("/delete_task/<task_id>")
+def delete_task(task_id):
+    mongo.db.tasks.remove({"_id": ObjectId(task_id)})
+    flash("Task Successfully Deleted")
+    return redirect(url_for("get_tasks"))
+
+
 @app.route("/logout")
 def logout():
     # remove user from session cookies
